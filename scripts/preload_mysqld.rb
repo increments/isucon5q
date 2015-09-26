2a76502a0a41152868d80b32800ae90667437aca
Yuji Nakayama <nkymyj@gmail.com>
2015-09-26 18:55:58 +0900

    Fix

--- scripts/preload_mysqld.rb
+++ scripts/preload_mysqld.rb
@@ -2,5 +2,5 @@ require 'shellwords'
 
 %w(comments entries new_footprints profiles relations users).each do |table|
   sql = "select sum(id) from #{table};"
-  system("mysql -u root isucon5q #{sql.shellescape}")
+  system("echo #{sql.shellescape} | mysql -u root isucon5q")
 end
