d96c05d2e291364e3c5a6ba9350d6205081babf6
Yuji Nakayama <nkymyj@gmail.com>
2015-09-26 12:22:36 +0900

    Use rack-mini-profiler

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'mysql2'
 require 'mysql2-cs-bind'
 require 'tilt/erubis'
 require 'erubis'
+require 'rack-mini-profiler'
 
 module Isucon5
   class AuthenticationError < StandardError; end
@@ -18,6 +19,7 @@ end
 
 class Isucon5::WebApp < Sinatra::Base
   use Rack::Session::Cookie
+  use Rack::MiniProfiler
   set :erb, escape_html: true
   set :public_folder, File.expand_path('../../static', __FILE__)
   #set :sessions, true
