f36336c530ec9d322f650d6a53d4bfb20330a7e4
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-09-26 03:28:07 +0000

    tiny impl

--- app.rb
+++ app.rb
@@ -314,7 +314,7 @@ SQL
     authenticated!
     query = 'INSERT INTO entries (user_id, private, body) VALUES (?,?,?)'
     body = (params['title'] || "タイトルなし") + "\n" + params['content']
-    db.xquery(query, current_user[:id], (params['private'] ? '1' : '0'), body)
+    db.xquery(query, current_user[:id], (params['private'] ? 1 : 0), body)
     redirect "/diary/entries/#{current_user[:account_name]}"
   end
 
